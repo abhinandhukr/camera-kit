00182284d5a9b0f7dfb7a778734b7c744c7bc9cf
abhinandhu <abhinandhu.kr@qapita.com>
2024-02-13 10:37:48 +0530

    style changes

--- camer-kit-demo/src/App.js
+++ camer-kit-demo/src/App.js
@@ -3,7 +3,7 @@ import Dashboard from './dashboard';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen overflow-hidden">
       <Dashboard/>
     </div>
   );
